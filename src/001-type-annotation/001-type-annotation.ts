b026932ad173f0c54fa004f9ea7db6e3ab55c8e1
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-04 12:10:24 -0300

    iniciando estudo de tipos

---
+++ src/001-type-annotation/001-type-annotation.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+// tipos basicos (inferência de tipos)
+const yourName: string = 'Cleber';
+const age: number = 27;
+const adult: boolean = true;
+const symbol: symbol = Symbol('my symbol');
+const big: bigint = 10n;
+
