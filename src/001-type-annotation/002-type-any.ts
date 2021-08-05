1a69acbfd9f3f7ea27e1eebe494e89dc7895ae49
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-05 10:57:27 -0300

    002 Type any

---
+++ src/001-type-annotation/002-type-any.ts
@@ -0,0 +1,6 @@
+// tipo any, é um tipo que pode receber qualquer coisa, não e esperado um tipo especifico.
+// esse tipo você irá utilizar somente em um ultimo caso.
+
+const message = (msg: any) => msg;
+
+console.log(message('Ola'));
