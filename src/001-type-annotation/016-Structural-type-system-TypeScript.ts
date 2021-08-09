705217dc371979af39452736b545294059c162b0
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-09 11:38:35 -0300

    Structural type 2

--- src/001-type-annotation/016-Structural-type-system-TypeScript.ts
+++ src/001-type-annotation/016-Structural-type-system-TypeScript.ts
@@ -1,4 +1,5 @@
 // Structural type é o sistema de tipos do TypeScript (Tipagem estruturada)
+// o typeScript não olha para identidade do tipo, e sim para sua estrutura
 
 type VefifyUser = boolean; // tipagem da função
 type User = string;
