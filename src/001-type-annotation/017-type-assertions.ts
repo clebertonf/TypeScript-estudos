c9ea07445e6f8a07c9c012573864b93297cb9644
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-09 12:09:58 -0300

    Type assertions 3 HTML element exemplo recomendado ou não

--- src/001-type-annotation/017-type-assertions.ts
+++ src/001-type-annotation/017-type-assertions.ts
@@ -1,16 +1,11 @@
+/** Recomendado */
+
 // Pode ser HTMLBodyElement ou nullo
 // Por isso precisa de uma verificação if
 // Utilize em casos que você não tem certeza que o elemento existe
 
 const body1 = document.querySelector('body');
 if (body1) body1.style.background = 'red';
-
-// Non-null assertion (!)
-// Especifico que não pode ser nullo
-// Utilize so em casos que voce tenha certeza que o elemento existe
-
-const body2 = document.querySelector('body')!;
-body2.style.background = 'red';
 
 // Type assertion
 // Especifico ao typeScript que o tipo deve ser HTMLBodyElement e não pode ser nullo
@@ -22,3 +17,21 @@ body3.style.background = 'red';
 // HTMLInputElement
 const input = document.getElementById('input-name') as HTMLInputElement;
 input.value = 'Seu nome';
+
+/** Não recomendado */
+
+// Non-null assertion (!)
+// Especifico que não pode ser nullo
+// Utilize so em casos que voce tenha certeza que o elemento existe
+
+const body2 = document.querySelector('body')!;
+body2.style.background = 'red';
+
+// Cuidado não e possivel colocar qualquer tipo, mas somente tipos pré definidos
+// isso pode até ser feito, mas e bem improvavel que precise
+// exemplo abaixo
+
+let input2 = (document.getElementById('input-idade') as unknown | any) as number;
+input2 = 18;
+
+console.log(input2);
