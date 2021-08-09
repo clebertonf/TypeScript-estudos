fcc36e8de921260dbf397271dea699e84dec3ffe
Cleberton Francisco <clebertonfgc@gmail.com>
2021-08-09 11:59:10 -0300

    Type assertions 2

--- src/001-type-annotation/017-type-assertions.ts
+++ src/001-type-annotation/017-type-assertions.ts
@@ -1,16 +1,20 @@
 // Pode ser HTMLBodyElement ou nullo
 // Por isso precisa de uma verificação if
+// Utilize em casos que você não tem certeza que o elemento existe
 
 const body1 = document.querySelector('body');
 if (body1) body1.style.background = 'red';
 
 // Non-null assertion (!)
 // Especifico que não pode ser nullo
+// Utilize so em casos que voce tenha certeza que o elemento existe
 
 const body2 = document.querySelector('body')!;
 body2.style.background = 'red';
 
 // Type assertion
 // Especifico ao typeScript que o tipo deve ser HTMLBodyElement e não pode ser nullo
+// Utilize so em casos que voce tenha certeza que o elemento existe
+
 const body3 = document.querySelector('body') as HTMLBodyElement;
 body3.style.background = 'red';
